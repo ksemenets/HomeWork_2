4d0806f72dde36adc7cb6bb55c868d30c5c8997c
Konstantin <ksemenec@mail.ru>
2022-12-22 21:50:10 +0500

    task10

---
+++ task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу,  которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+
+Console.WriteLine("Введите 3-х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = 0;
+
+ number1 = (number / 10) % 10;
+
+ if (number / 10 >= 10 & number /10 < 100)
+    Console.WriteLine ($"Вторая цифра этого числа {number1}");
+ else   
+    Console.WriteLine ("Ну тебе же сказали, 3-Х ЗНАЧНОЕ");
